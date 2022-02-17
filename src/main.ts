9df150d28b3403d6dcfac09bccb23e8754a82aed
YangGuangLiang <yangguangliang@bytedance.com>
2022-02-17 17:43:03 +0800

    gDesign test

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import gDesign from 'g-design'
+console.log(gDesign)
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(gDesign).mount('#app')
